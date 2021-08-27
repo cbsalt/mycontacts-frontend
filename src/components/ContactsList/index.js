34538651b3e9afcb5d557a6f3bbb7500ecb412d2
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-08-27 18:22:20 -0300

    feat: contacts components and colors default

---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,67 @@
+import {
+  Card, Container, Header, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Charles</strong>
+              <small>family</small>
+            </div>
+            <span>charles@mail.com</span>
+            <span>(99) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Charles</strong>
+              <small>family</small>
+            </div>
+            <span>charles@mail.com</span>
+            <span>(99) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
