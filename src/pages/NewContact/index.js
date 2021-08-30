7118cbd9eb92821a4898ef1a2123e3bdede595f3
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-08-30 13:29:54 -0300

    feat: button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,24 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input placeholder="bla bla" />
+      <Select>
+        <option value="1">Family</option>
+        <option value="2">Hunsrik</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+    </>
   );
 }
