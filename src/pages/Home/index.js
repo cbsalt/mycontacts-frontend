767c76753c87645b23001e534662467f5c6d33bd
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2022-05-29 17:26:55 -0300

    feat: search not found

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -23,6 +24,7 @@ import {
   ListHeader,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 
 export default function Home() {
@@ -130,6 +132,14 @@ export default function Home() {
                 cadastrar o seu primeiro!
               </p>
             </EmptyListContainer>
+          )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier question" />
+
+              <span>Nenhum resultado foi encontrado para <b>{searchTerm}</b>.</span>
+            </SearchNotFoundContainer>
           )}
 
           {filteredContacts.length > 0 && (
