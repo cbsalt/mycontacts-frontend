05616fc382ffe55830504eb346bb622f9d08cb5e
Charles <cbsal.tec@gmail.com>
2022-11-15 16:13:37 -0300

    feat: use toast component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,12 +8,15 @@ import Routes from '../../Routes';
 import Header from '../Header';
 
 import { Container } from './styles';
+import ToastContainer from '../Toast/ToastContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
+        <ToastContainer />
+
         <Container>
           <Header />
           <Routes />
