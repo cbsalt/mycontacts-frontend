396a2374e4b83c4423a202a6f8e4a59b4b28b667
Charles <cbsal.tec@gmail.com>
2023-07-21 13:08:11 -0300

    feat(toast): cleaning toast after timeout

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
@@ -8,6 +9,16 @@ import { Container } from './styles';
 export default function ToastMessage({
   message, onRemoveMessage,
 }) {
+  useEffect(() => {
+    const setTimeoutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 5000);
+
+    return () => {
+      clearTimeout(setTimeoutId);
+    };
+  }, [message, onRemoveMessage]);
+
   function handleRemoveToast() {
     onRemoveMessage(message.id);
   }
@@ -31,6 +42,7 @@ ToastMessage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['default', 'success', 'danger']),
+    duration: PropTypes.number,
   }).isRequired,
   onRemoveMessage: PropTypes.func.isRequired,
 };
